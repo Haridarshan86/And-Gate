c6cdaa94af9a53d281096687510e793820f2d202
Haridarshan B Kunder <darshanhari86@gmail.com>
2026-05-27 18:39:37 +0530

    Add files via upload

---
+++ And_Gate.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 27.05.2026 18:08:18
+// Design Name: 
+// Module Name: And_Gate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module And_Gate(input a,b,output y);
+assign y=a&b;
+endmodule
---
+++ And_Gate_tb.v
@@ -0,0 +1,65 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 27.05.2026 18:15:33
+// Design Name: 
+// Module Name: And_Gate_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+//=================================================
+// Testbench for 2-Input AND Gate
+// File: and_gate_tb.v
+//=================================================
+
+
+module and_gate_tb;
+
+reg A;
+reg B;
+wire Y;
+
+// Instantiate the AND gate module
+and_gate dut (
+    a,b,y);
+
+
+initial begin
+
+    // Test Case 1
+    A = 0; B = 0;
+    #10;
+    $display("%b %b | %b", A, B, Y);
+
+    // Test Case 2
+    A = 0; B = 1;
+    #10;
+    $display("%b %b | %b", A, B, Y);
+
+    // Test Case 3
+    A = 1; B = 0;
+    #10;
+    $display("%b %b | %b", A, B, Y);
+
+    // Test Case 4
+    A = 1; B = 1;
+    #10;
+    $display("%b %b | %b", A, B, Y);
+
+    $finish;
+end
+
+endmodule
